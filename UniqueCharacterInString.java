7ab16b2ed29d8410fe4e2625bf759dd95f111a31
Abhra Kar <abhra.kar@gmail.com>
2017-08-04 12:20:03 +0530

    Update UniqueCharacterInString.java

--- UniqueCharacterInString.java
+++ UniqueCharacterInString.java
@@ -1,8 +1,6 @@
 public class UniqueCharacterInString {
 	public static void main(String[] args) {
-
 		String str = "abcdefsredgebc";
-
 		for (int i = 0; i < str.length(); i++) {
 			if (str.indexOf(str.charAt(i)) == str.lastIndexOf(str.charAt(i))) {
 				System.out.print(str.charAt(i) + " ");
