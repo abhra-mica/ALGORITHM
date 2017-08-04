b0cfd6fd998b30d0f321d6d4fdc09eafd4c9f8d7
Abhra Kar <abhra.kar@gmail.com>
2017-08-04 12:19:03 +0530

    Find unique character in a string

---
+++ UniqueCharacterInString.java
@@ -0,0 +1,12 @@
+public class UniqueCharacterInString {
+	public static void main(String[] args) {
+
+		String str = "abcdefsredgebc";
+
+		for (int i = 0; i < str.length(); i++) {
+			if (str.indexOf(str.charAt(i)) == str.lastIndexOf(str.charAt(i))) {
+				System.out.print(str.charAt(i) + " ");
+			}
+		}
+	}
+}
