1eabeb802cb9a46ba2b1f4a60afba55a6e0584a0
Abhra Kar <abhra.kar@gmail.com>
2017-08-05 09:05:31 +0530

    Add files via upload

---
+++ Dutch_National_Flag_Probkem.java
@@ -0,0 +1,36 @@
+/*Segregate 0'1, 1's and 2's from an array
+ */
+public class Dutch_National_Flag_Probkem {
+
+	public static void main(String[] args) {
+		int[] arr = { 0, 1, 1, 0, 1, 2, 1, 2, 0, 0, 0, 1,2 };
+
+		int low = 0, high = arr.length - 1, mid = 0;
+		while (mid <= high) {
+			switch (arr[mid]) {
+			case 0:
+				int temp = arr[low];
+				arr[low] = arr[mid];
+				arr[mid] = temp;
+				low++;
+				mid++;
+				break;
+
+			case 1:
+				mid++;
+				break;
+
+			case 2:
+				int temp1 = arr[high];
+				arr[high] = arr[mid];
+				arr[mid] = temp1;
+				high--;
+				break;
+			}
+		}
+		
+		for(int i : arr)
+			System.out.print(i +" ");
+	}
+
+}
