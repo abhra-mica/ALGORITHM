e0ebf15c4cbde98af40df175a5b21eb0b7369e20
Abhra Kar <abhra.kar@gmail.com>
2017-08-18 10:16:37 +0530

    Update CommomCharInTwoStrings.java

--- CommomCharInTwoStrings.java
+++ CommomCharInTwoStrings.java
@@ -1,26 +1,24 @@
 import java.util.Set;
 import java.util.TreeSet;
-
+//The code is developed assuming character are small letters only.
 public class CommomCharInTwoStrings {
 	public static void main(String[] args) {
 		String s1 = "abcdefgh";
 		String s2 = "bdfglkknhui";
-
-		boolean[] occ = new boolean[26];
+		boolean[] charOccurence = new boolean[26];
 
 		for (int i = 0; i < s1.length(); i++) {
-			occ[s1.charAt(i) % 97] = true;
+			charOccurence[s1.charAt(i) % 97] = true;
 		}
 
 		Set<Character> set = new TreeSet<>();
 		for (int i = 0; i < s2.length(); i++) {
-			if (occ[s2.charAt(i) % 97] == true) {
+			if (charOccurence[s2.charAt(i) % 97] == true) {
 				set.add(s2.charAt(i));
 			}
 		}
 
-		for (char s : set) {
+		for (char s : set) 
 			System.out.print(s+ " ");
-		}
 	}
 }
