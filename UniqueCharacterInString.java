1777cbf21687e02b5352889b4584cc4a0aa306fe
Abhra Kar <abhra.kar@gmail.com>
2017-08-18 10:20:29 +0530

    Update UniqueCharacterInString.java

--- UniqueCharacterInString.java
+++ UniqueCharacterInString.java
@@ -2,6 +2,8 @@ public class UniqueCharacterInString {
 	public static void main(String[] args) {
 		String str = "abcdefsredgebc";
 		for (int i = 0; i < str.length(); i++) {
+			//Index of returns first occurance of char and lastIndexOf returns last index of char.If 
+			//these two returns values are same then characters exists only one time.
 			if (str.indexOf(str.charAt(i)) == str.lastIndexOf(str.charAt(i))) {
 				System.out.print(str.charAt(i) + " ");
 			}
