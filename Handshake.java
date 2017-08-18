493a518bb0b5bc324372270d126a6dc2807fd2ec
Abhra Kar <abhra.kar@gmail.com>
2017-08-18 10:22:07 +0530

    Update Handshake.java

--- Handshake.java
+++ Handshake.java
@@ -1,5 +1,5 @@
-/*In a party n number of guests are available.How many no. of handshake will happens between them.
-Ans:-One guest handshake with exactly n-1 no. of guests.So all individuals guests will handshake with other n-1 no. of guests.
+/*In a party n number of guests are available.How many no. of handshake will happens between them?
+Ans:-One guest handshake with exactly (n-1) no. of guests.So all individuals guests will handshake with other (n-1) no. of guests.
 So total n*(n-1) no. of handshaking will happens.Now A handshakes with B and B handshakes with A is same.
 So total no. of handshakes is n*(n-1)/2.*/
 
