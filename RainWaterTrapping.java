96591ffc0435da2cb2658a8d658b71f877198669
Abhra Kar <abhra.kar@gmail.com>
2017-08-18 17:21:19 +0530

    Update RainWaterTrapping.java

--- RainWaterTrapping.java
+++ RainWaterTrapping.java
@@ -2,11 +2,12 @@ public class RainWaterTrapping {
 
 	public static void main(String[] args) {
 		int[] towerHeights = { 1, 5, 2, 3, 1, 7, 2, 4 };
-		int[] rightMaxHeight = new int[towerHeights.length];//This array used to store right side maximum height respective to every building
-
+		int[] rightMaxHeight = new int[towerHeights.length];//This array used to store right side maximum height respective 
+								     //to every building
 		int maxHeightTillNow = 0;
 
-		//THe right most building don't have any building right after that,we are taking right most building height as his right max height.
+		//THe right most building don't have any building right after that,we are taking right most building height as
+					//his right max height.
 		for (int i = towerHeights.length - 1; i >= 0; i--) {
 			if (towerHeights[i] > maxHeightTillNow) {
 				maxHeightTillNow = towerHeights[i];
